3b516d6d5e9967378a345692cf912dc49821da32
xiaofei <alibaba22001@163.com>
2021-11-28 14:02:48 +0800

    update

--- apps/channels.c
+++ apps/channels.c
@@ -6,7 +6,7 @@
  *   文件名称：channels.c
  *   创 建 者：肖飞
  *   创建日期：2020年06月18日 星期四 09时23分30秒
- *   修改日期：2021年11月04日 星期四 11时02分59秒
+ *   修改日期：2021年11月28日 星期日 13时58分55秒
  *   描    述：
  *
  *================================================================*/
@@ -3283,7 +3283,7 @@ static int channels_set_channels_config(channels_info_t *channels_info, channels
 	channels_config->power_module_config.channels_power_module_number = channels_info->power_module_item_number;
 	channels_config->channel_number = channels_info->channel_number;
 
-	channels_info->power_modules_info = get_or_alloc_power_modules_info(channels_info->channels_config);
+	channels_info->power_modules_info = alloc_power_modules_info(channels_info->channels_config);
 	OS_ASSERT(channels_info->power_modules_info != NULL);
 	power_modules_set_type(channels_info->power_modules_info, channels_settings->power_module_type);
 
